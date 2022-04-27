285bc4ccca2a12584b91e23f74efbb9aafa03075
Webbmanpy <104485613+Webbmanpy@users.noreply.github.com>
2022-04-27 12:07:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+import json
+
+
+
+st.set_page_config(page_title="For mummy", page_icon=":tada:", layout="wide")
+
+st.subheader('Happy Birthday Mummy! look I made a website!'
+             '')
+st.title('I love you')
+st.write("Thankyou so much for raising me and thinking about my needs and loving me. You are the best mother I love "
+         "you the most")
+
+
+
+
+
+
+st.title("My E-cards")
+st.image("Happy b'day (1).jpg")
+st.image("I LOVE U.jpg")
+
+st.title("YOU ARE SPECIAL")
